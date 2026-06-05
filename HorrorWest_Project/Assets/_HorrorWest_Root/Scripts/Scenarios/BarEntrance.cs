7db0672539dae669602f0dfb50c1823307bc76e0
DavidSaez21 <david04mingla@gmail.com>
2026-06-05 17:26:12 +0200

    carcel

--- HorrorWest_Project/Assets/_HorrorWest_Root/Scripts/Scenarios/BarEntrance.cs
+++ HorrorWest_Project/Assets/_HorrorWest_Root/Scripts/Scenarios/BarEntrance.cs
@@ -6,15 +6,19 @@ public class BarEntrance : MonoBehaviour
     [SerializeField] private GameObject streetTilemap;
     [SerializeField] private GameObject streetProps;
 
-    [Header("Bar")]
+    [Header("Bar / Interior")]
     [SerializeField] private GameObject barTilemap;
     [SerializeField] private GameObject barProps;
     [SerializeField] private Transform playerSpawnInside;
     [SerializeField] private GameObject doorBlocker;
 
-    [Header("Límites cámara bar")]
-    [SerializeField] private Vector2 barCamMin;   // esquina inf izq del bar
-    [SerializeField] private Vector2 barCamMax;   // esquina sup der del bar
+    [Header("Confiner calle")]
+    [SerializeField] private Vector2 streetCamMin;
+    [SerializeField] private Vector2 streetCamMax;
+
+    [Header("Confiner interior")]
+    [SerializeField] private Vector2 barCamMin;
+    [SerializeField] private Vector2 barCamMax;
 
     private bool _insideBar = false;
     private CameraConfiner _confiner;
@@ -26,6 +30,10 @@ public class BarEntrance : MonoBehaviour
         if (doorBlocker != null) doorBlocker.SetActive(false);
 
         _confiner = Camera.main.GetComponent<CameraConfiner>();
+
+        // Activa el confiner del pueblo al inicio
+        if (_confiner != null)
+            _confiner.SetBounds(streetCamMin, streetCamMax);
     }
 
     public void EnterBar()
@@ -44,6 +52,7 @@ public class BarEntrance : MonoBehaviour
         if (playerSpawnInside != null)
             PlayerManager.Instance.transform.position = playerSpawnInside.position;
 
+        // Cambia al confiner del interior
         if (_confiner != null)
             _confiner.SetBounds(barCamMin, barCamMax);
     }
