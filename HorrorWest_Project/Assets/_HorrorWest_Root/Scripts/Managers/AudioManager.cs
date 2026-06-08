80ebec1f58e0d00ffabcb8cc5d0cd758d997a872
RubiHD <rubi.de.rankawa@gmail.com>
2026-06-08 11:04:08 +0200

    Banda sonora y conexion ajustes

--- HorrorWest_Project/Assets/_HorrorWest_Root/Scripts/Managers/AudioManager.cs
+++ HorrorWest_Project/Assets/_HorrorWest_Root/Scripts/Managers/AudioManager.cs
@@ -3,32 +3,67 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("SFX")]
     public Slider sfxSlider;
     private float _sfxVolume = 1f;
 
+    [Header("Música")]
+    public Slider musicSlider;
+    public AudioSource musicSource;
+    private float _musicVolume = 1f;
+
     void Start()
     {
-        float savedVolume = PlayerPrefs.GetFloat("sfxVolume", 1f);
-        _sfxVolume = savedVolume;
+        // SFX
+        float savedSFX = PlayerPrefs.GetFloat("sfxVolume", 1f);
+        _sfxVolume = savedSFX;
 
         if (sfxSlider != null)
         {
             sfxSlider.onValueChanged.RemoveAllListeners();
-            sfxSlider.value = savedVolume;
+            sfxSlider.value = savedSFX;
             sfxSlider.onValueChanged.AddListener(OnSFXVolumeChanged);
         }
+
+        // Música
+        float savedMusic = PlayerPrefs.GetFloat("musicVolume", 1f);
+        _musicVolume = savedMusic;
+
+        if (musicSlider != null)
+        {
+            musicSlider.onValueChanged.RemoveAllListeners();
+            musicSlider.value = savedMusic;
+            musicSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        }
+
+        if (musicSource != null)
+            musicSource.volume = _musicVolume;
     }
 
     public void OnSFXVolumeChanged(float value)
     {
-        Debug.Log("Volumen cambiado a: " + value);
         _sfxVolume = value;
         PlayerPrefs.SetFloat("sfxVolume", value);
         PlayerPrefs.Save();
     }
 
+    public void OnMusicVolumeChanged(float value)
+    {
+        _musicVolume = value;
+        PlayerPrefs.SetFloat("musicVolume", value);
+        PlayerPrefs.Save();
+
+        if (musicSource != null)
+            musicSource.volume = value;
+    }
+
     public float GetSFXVolume()
     {
         return _sfxVolume;
+    }
+
+    public float GetMusicVolume()
+    {
+        return _musicVolume;
     }
 }
